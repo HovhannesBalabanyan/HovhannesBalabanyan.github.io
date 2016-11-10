8779a20400ebd704ac2aca50d96c91cea816a3a6
HovhannesBalabanyan <hyehovo32@yahoo.com>
2016-11-09 17:50:02 -0800

    lot of additions and edits

--- fall16/Project3/js/main.js
+++ fall16/Project3/js/main.js
@@ -1,1 +1,12 @@
-$('.collapse').collapse()
+function toggle5(showHideDiv, switchImgTag) {
+        var ele = document.getElementById(showHideDiv);
+        var imageEle = document.getElementById(switchImgTag);
+        if(ele.style.display == "block") {
+                ele.style.display = "none";
+		imageEle.innerHTML = '<img src="/wp-includes/images/plus.png">';
+        }
+        else {
+                ele.style.display = "block";
+                imageEle.innerHTML = '<img src="/wp-includes/images/minus.png">';
+        }
+}
